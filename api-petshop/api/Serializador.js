d590276d1975f99921fc815f593352362dceec0c
frigo27 <frigo34@hotmail.com>
2020-09-21 13:47:12 -0300

    erro melhorias

---
+++ api-petshop/api/Serializador.js
@@ -0,0 +1,18 @@
+const { ne } = require("sequelize/types/lib/operators")
+
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+
+class Serializador {
+  json (dados) {
+    return JSON.stringify(dados)    
+  }
+
+  serializar(dados) {
+    if (this.contentType === 'application/json') {
+      return this.json(dados)
+    }
+
+    throw new ValorNaoSuportado(this.contentType)
+
+  }
+}
